8cf68b2e9e5f9f90eeb59e7cfd3e723ca33ecf94
austencloud <austencloud@gmail.com>
2025-08-20 15:31:47 -0500

    Add .netlify/ to .gitignore and remove from tracking

--- web/.netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DojEQME0.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/BtI6aIiM.js","_app/immutable/chunks/IUYm_0rb.js","_app/immutable/chunks/DVUHJB5u.js","_app/immutable/chunks/Cukrbc5R.js","_app/immutable/chunks/B5I7GX32.js"];
-export const stylesheets = [];
-export const fonts = [];
--- web/.netlify/server/nodes/8.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 8;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/getting-started/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/8.CDoDBHvC.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/BtI6aIiM.js","_app/immutable/chunks/IUYm_0rb.js","_app/immutable/chunks/B5I7GX32.js","_app/immutable/chunks/DVUHJB5u.js","_app/immutable/chunks/Q46znR_b.js","_app/immutable/chunks/C8ay5gD4.js","_app/immutable/chunks/C-eppkXD.js","_app/immutable/chunks/DAUS0jAM.js","_app/immutable/chunks/C-ElHC__.js"];
-export const stylesheets = ["_app/immutable/assets/AboutTab.XL5PghD6.css"];
-export const fonts = [];
